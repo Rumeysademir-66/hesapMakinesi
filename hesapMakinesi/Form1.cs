bf6e0ac3bc5dcfcdb6875fd9d8d7c35cbb8d5798
RÜMEYSA <RMEYSA@DESKTOP-82JF44B>
2026-05-19 13:07:58 +0300

    Proje dosyası ekle.

---
+++ hesapMakinesi/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hesapMakinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sayi1 = Convert.ToDouble(textBox1.Text);
+            double sayi2 = Convert.ToDouble(textBox2.Text); 
+            double sum= sayi1 + sayi2;
+            textBox3.Text = sum.ToString(); 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double sayi1 = Convert.ToDouble(textBox1.Text);
+            double sayi2 = Convert.ToDouble(textBox2.Text);
+            double diff = sayi1 - sayi2;
+            textBox3.Text = diff.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            double sayi1 = Convert.ToDouble(textBox1.Text);
+            double sayi2 = Convert.ToDouble(textBox2.Text);
+            double division = sayi1 / sayi2;
+            textBox3.Text = division.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double sayi1 = Convert.ToDouble(textBox1.Text);
+            double sayi2 = Convert.ToDouble(textBox2.Text);
+            double product = sayi1 * sayi2;
+            textBox3.Text = product.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double sayi1 = Convert.ToDouble(textBox1.Text);
+            double sayi2 = Convert.ToDouble(textBox2.Text);
+            double percent = (sayi1 / sayi2) * 100;
+            textBox3.Text = percent.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
